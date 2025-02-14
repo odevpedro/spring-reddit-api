164e54581df916aba0c69b939f1bc7960ad15afa
odevpedro <pedrosschmidt2@gmail.com>
2025-02-14 19:39:45 -0300

    comments crud

---
+++ src/main/java/com/example/springredditclone/service/CommentService.java
@@ -0,0 +1,73 @@
+package com.example.springredditclone.service;
+
+import com.example.springredditclone.dto.CommentsDto;
+import com.example.springredditclone.exceptions.PostNotFoundException;
+import com.example.springredditclone.exceptions.SpringRedditException;
+import com.example.springredditclone.mapper.CommentMapper;
+import com.example.springredditclone.model.Comment;
+import com.example.springredditclone.model.NotificationEmail;
+import com.example.springredditclone.model.Post;
+import com.example.springredditclone.model.User;
+import com.example.springredditclone.repository.CommentRepository;
+import com.example.springredditclone.repository.PostRepository;
+import com.example.springredditclone.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Service
+@AllArgsConstructor
+public class CommentService {
+
+    private static final String POST_URL = "";
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+    private final AuthService authService;
+    private final CommentMapper commentMapper;
+    private final CommentRepository commentRepository;
+    private final MailContentBuilder mailContentBuilder;
+    private final MailService mailService;
+
+    public void save(CommentsDto dto) {
+        Post post = postRepository.findById(dto.getPostId()).orElseThrow(() -> new
+                PostNotFoundException(dto.getPostId().toString()));
+        Comment comment = commentMapper.map(dto, post, authService.getCurrentUser());
+        commentRepository.save(comment);
+
+        String message = mailContentBuilder.build(post.getUser().getUsername() + "Post a comment on your post" + POST_URL);
+        sendCommentNotification(message, post.getUser());
+
+    }
+
+    private void sendCommentNotification(String message, User user){
+        mailService.sendMail(new NotificationEmail(user.getUsername() + "Commented on your post", user.getEmail(), user.getUsername()));
+    }
+
+    public List<CommentsDto> getAllCommentsForPost(Long postId) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> new PostNotFoundException(postId.toString()));
+        return commentRepository.findByPost(post)
+                .stream()
+                .map(commentMapper::mapToDto).collect(toList());
+    }
+
+    public List<CommentsDto> getAllCommentsForUser(String userName) {
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new UsernameNotFoundException(userName));
+        return commentRepository.findAllByUser(user)
+                .stream()
+                .map(commentMapper::mapToDto)
+                .collect(toList());
+    }
+
+    public boolean containsSwearWords(String comment) {
+        if (comment.contains("shit")) {
+            throw new SpringRedditException("Comments contains unacceptable language");
+        }
+        return false;
+    }
+}
